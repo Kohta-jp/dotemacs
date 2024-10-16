722ef983f434420a54671adf5bcea2043c467db6
Kindai_Desktop_K.Nakajima <shimasanlog.d@gmail.com>
2024-10-16 20:27:39 +0900

    github copilot remove --cache .cache/ el-get/

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,8 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("9fb561389e5ac5b9ead13a24fb4c2a3544910f67f12cfcfe77b75f36248017d0" "51ec7bfa54adf5fff5d466248ea6431097f5a18224788d0bd7eb1257a4f7b773" "ae42678884b6f803215a3f8a2e201624515cef1969d3844e9be6c43b8d4fc7a6" default)))
+ '(package-selected-packages
+   '(auto-async-byte-compile multi-term exec-path-from-shell markdown-preview-mode markdown-mode nerd-icons neotree company flycheck zenburn-theme dash s editorconfig marginalia vertico orderless consult doom-modeline blackout el-get hydra leaf-keywords leaf)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
